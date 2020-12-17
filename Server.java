2791f7aaa040f7653d7ba3e1b02dca29b45b2936
Alexey Tarabanov <tarabanov@bk.ru>
2020-12-17 23:34:52 +0300

    1/22

---
+++ Server.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task30.task3008;
+
+/**
+ * 1. Создал классы ConsoleHelper, Connection, Message, MessageType, Server.
+ *    В последнем вызвал метод main
+ *
+ */
+
+// основной класс сервера
+public class Server {
+    public static void main(String[] args) {
+
+    }
+}
