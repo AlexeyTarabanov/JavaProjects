a5871ca76a26d55d038ee6cd9885623136337c26
Alexey Tarabanov <tarabanov@bk.ru>
2020-12-17 23:43:51 +0300

    3/22

--- MessageType.java
+++ MessageType.java
@@ -1,5 +1,15 @@
 package com.javarush.task.task30.task3008;
 
+/**
+ * 1) NAME_REQUEST - запрос имени.
+ * 2) USER_NAME - имя пользователя.
+ * 3) NAME_ACCEPTED - имя принято.
+ * 4) TEXT - текстовое сообщение.
+ * 5) USER_ADDED - пользователь добавлен.
+ * 6) USER_REMOVED - пользователь удален.
+ * */
+
 // отвечает за тип сообщений пересылаемых между клиентом и сервером
 public enum MessageType {
+    NAME_REQUEST, USER_NAME, NAME_ACCEPTED, TEXT, USER_ADDED, USER_REMOVED
 }
--- Server.java
+++ Server.java
@@ -4,7 +4,7 @@ package com.javarush.task.task30.task3008;
  * 1. Создал классы ConsoleHelper, Connection, Message, MessageType, Server.
  *    В последнем вызвал метод main
  * 2. Реализлвал класс ConsoleHelper.
- *
+ * 3. Добавил значения в enum MessageType.
  */
 
 // основной класс сервера
