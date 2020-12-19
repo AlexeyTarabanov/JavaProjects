d67b5677680fa15a10c2a9ae24567a6c4bfaa481
Alexey Tarabanov <tarabanov@bk.ru>
2020-12-20 00:30:20 +0300

    7/22

--- Server.java
+++ Server.java
@@ -3,39 +3,66 @@ package com.javarush.task.task30.task3008;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * 1. Создал классы ConsoleHelper, Connection, Message, MessageType, Server.
- * В последнем вызвал метод main
- * 2. Реализлвал класс ConsoleHelper.
+ *    - в последнем вызвал метод main
+ * 2. В класс ConsoleHelper добавил:
+ *    - поле reader и сразу инициализировал его
+ *    - метод writeMessage(String message), который выводит сообщение message в консоль.
+ *      реализовал его.
+ *    - методы int readInt() / String readString(), которые возвращают число / строку, считанные с консоли
  * 3. Добавил значения в enum MessageType.
- * 4. Реализовал класс Message
- * 5. Реализовал класс Connection.
+ * 4. В класс Message добавил:
+ *    - добавил поддержку Serializable
+ *    - поля MessageType type, String data, геттеры для них и контструкторы.
+ * 5. В класс Connection добавил:
+ *    - поля Socket socket, ObjectOutputStream out, ObjectInputStream in и конструктор
+ *    - метод send(Message message), который будет отправлять сообщения (сериализовать)
+ *    - метод receive(), который читает сообщение (десериализовывает)
+ *    - метод SocketAddress getRemoteSocketAddress(), который возвращает удаленный адрес сокетного соединения
+ *    - метод close(), который закрывает все ресурсы класса
+ * 6. В класс Server добавил:
+ *    - статический вложенный класс Handler, унаследовал его от Thread
+ *      В класс Handler добавил:
+ *        - поле Socket socket и конструктор
+ *    в методе main:
+ *    - считываю с клавиатуры порт сервера
+ *    - создал серверный сокет на основе полученного порта
+ *    - слушаем и принимаем входящие сокетные соединения
+ *    - создаю и сразу запускаю новый поток Handler
+ * 7. В класс Server добавил:
+ *    - поле connectionMap, где ключом будет имя клиента, а значением - соединение с ним
+ *    - метод sendBroadcastMessage, который должен отправлять сообщение всем соединениям из connectionMap
  */
 
-/*
-* Сервер должен поддерживать множество соединений с разными клиентами одновременно.
-Это можно реализовать с помощью алгоритма:
-- Сервер создает серверное сокетное соединение.
-- В цикле ожидает, когда какой-то клиент подключится к сокету.
-- Создает новый поток обработчик Handler, в котором будет происходить обмен сообщениями с клиентом.
-- Ожидает следующее соединение.
-* */
 
 // основной класс сервера
 public class Server {
+
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
+
+    public static void sendBroadcastMessage(Message message) {
+        try {
+            for (Connection connection : connectionMap.values()) {
+                connection.send(message);
+            }
+        } catch (IOException e) {
+            ConsoleHelper.writeMessage("Сообщение не отправлено");
+        }
+    }
+
     public static void main(String[] args) {
 
-        // запрашиваем порт сервера
         int port = ConsoleHelper.readInt();
-        try // создем серверный сокет на основе полученного порта
-            (ServerSocket serverSocket = new ServerSocket(port))
-        {
+
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
             ConsoleHelper.writeMessage("Server is running...");
             while (true) {
-                // слушаем и принимаем входящие сокетные соединения
                 Socket client = serverSocket.accept();
-                // создаем и запускаем новый поток Handler
                 new Handler(client).start();
             }
         } catch (IOException e) {
@@ -44,6 +71,7 @@ public class Server {
     }
 
     private static class Handler extends Thread {
+
         private Socket socket;
 
         public Handler(Socket socket) {
